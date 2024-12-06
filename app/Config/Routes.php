5f5d2521ca85ed8c4521e3b5674fd8038b4e3712
Kurniawan-dev <kurniawanbrave27@gmail.com>
2024-12-06 14:50:55 +0700

    second commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,11 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
 $routes->get('/home', 'Home::index');
 $routes->get('/news', 'News::index');
 $routes->get('/news/(:any)', 'News::viewNews/$1');
 
+// CRUD di halaman Admin
 $routes->group('admin', function($routes){
 	$routes->get('news', 'NewsAdmin::index');
 	$routes->get('news/(:segment)/preview', 'NewsAdmin::preview/$1');
